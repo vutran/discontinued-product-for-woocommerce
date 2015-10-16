716cad87b21eddd4c955167620775dd06e7ac14f
vutran <vutran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-16 20:24:50 +0000

    initial commit
    git-svn-id: http://plugins.svn.wordpress.org/discontinued-product-for-woocommerce/trunk@1267628 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Plugin Name ===
+Contributors: vutran
+Donate link: http://example.com/
+Tags: woocommerce, e-commerce, ecommerce, shopping, discontinued, products, inventory
+Requires at least: 3.0.1
+Tested up to: 4.3.1
+Stable tag: 0.1.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Adds the ability to flag a product as discontinued to WooCommerce
+
+== Description ==
+
+This plugin adds the ability to mark a product in your WooCommerce inventory as discontinued.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Unzip the `.zip` file into the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 0.1.0 =
+* Iniital release
---
+++ woocommerce-discontinued-products.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: WooCommerce Discontinued Products
+Plugin URI: http://vu-tran.com/
+Description: Adds the ability to flag a product as discontinued to WooCommerce
+Version: 0.1.0
+Author: Vu Tran
+Author URI: http://vu-tran.com
+Copyright: © 2015 Vu Tran
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+add_action( 'init', 'woocommerce_discontinued_product_init' );
+add_action( 'woocommerce_product_options_general_product_data', 'woocommerce_discontinued_product_add_custom_fields' );
+add_action( 'woocommerce_process_product_meta', 'woocommerce_discontinued_product_save_custom_fields' );
+
+/**
+ * Initializes the plugin
+ *
+ * @access public
+ * @return void
+ */
+function woocommerce_discontinued_product_init()
+{
+    register_post_status( 'wc-discontinued', array(
+        'label' => __( 'Discontinued', 'woocommerce_discontinued_product' ),
+        'public' => true,
+        'exclude_from_search' => true,
+        'show_in_admin_all_list' => true,
+        'show_in_admin_status_list' => true,
+        'label_count' => _n_noop( 'Discontinued <span class="count">(%s)</span>', 'Discontinued <span class="count">(%s)</span>' )
+    ));
+}
+
+/**
+ * Adds the custom fields
+ *
+ * @access public
+ * @return void
+ */
+function woocommerce_discontinued_product_add_custom_fields()
+{
+    global $woocommerce, $post;
+    echo '<div class="options_group">';
+    woocommerce_wp_checkbox(array(
+        'id' => '_discontinued',
+        'label' => __( 'Discontinued', 'woocommerce_discontinued_product' ),
+        'name' => '_discontinued',
+        'description' => __( 'Check this box to mark this product as discontinued.', 'woocommerce_discontinued_product' )
+    ));
+    echo '</div>';
+}
+
+/**
+ * Saves the custom fields
+ *
+ * Saves the post meta "_discontinued" to "yes" or "no"
+ * Updates the post_status to "wc-discontinued" or "publish"
+ *
+ * @access public
+ * @param int $post_id          The post ID that is being edited
+ * @return void
+ */
+function woocommerce_discontinued_product_save_custom_fields( $post_id )
+{
+    $discontinued = isset( $_POST['_discontinued'] ) ? esc_attr( $_POST['_discontinued'] ) : null;
+    // Save the post meta
+    update_post_meta( $post_id, '_discontinued', esc_attr( $discontinued ) );
+    // Set the new post status
+    $newPostStatus = $discontinued ? 'wc-discontinued' : 'publish';
+    // Udpate the post status
+    wp_update_post(array(
+        'ID' => $post_id,
+        'post_status' => $newPostStatus
+    ));
+}
